22c59a1dcf78f94a88e404eda6ecf56f3bf108c3
HP <HP@DESKTOP-G4J7E8F>
2022-07-21 18:09:18 +0530

    Add project files.

---
+++ WebUserControl/UserControl.ascx
@@ -0,0 +1,3 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="UserControl.ascx.cs" Inherits="WebUserControl.UserControl" %>
+<asp:FileUpload ID="FileUpload1" runat="server" />
+<asp:Button ID="btnUpload" runat="server" Text="Upload" OnClick="btnUpload_Click" />
---
+++ WebUserControl/UserControl.ascx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebUserControl
+{
+    public partial class UserControl : System.Web.UI.UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnUpload_Click(object sender, EventArgs e)
+        {
+            string path = Server.MapPath("~/");
+            FileUpload1.SaveAs(path + FileUpload1.FileName);
+        }
+    }
+}
